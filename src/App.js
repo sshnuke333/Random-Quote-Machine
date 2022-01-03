f11f749e0c09fa721b9037c79b66dddf077f00a1
sshnuke333 <sshnuke@protonmail.com>
2022-01-03 20:45:42 +0530

    Code Refactor to pass user stories

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div id="quote-box">
+            <p id="text"></p>
+            <p id="author"></p>
+            <button id="new-quote"></button>
+            <a id="tweet-quote" href="twitter.com/intent/tweet"></a>
+        </div>
+    );
+}
