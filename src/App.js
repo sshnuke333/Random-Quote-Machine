ca8df9c7bf54546161508e03430dad26af9b5838
sshnuke333 <sshnuke@protonmail.com>
2022-01-03 21:18:56 +0530

    Code Refactor: Button Component, passing user stories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Button from './components/Button';
 
 export default function App() {
     return (
         <div id="quote-box">
             <p id="text"></p>
             <p id="author"></p>
-            <button id="new-quote"></button>
+            <Button />
             <a id="tweet-quote" href="twitter.com/intent/tweet"></a>
         </div>
     );
